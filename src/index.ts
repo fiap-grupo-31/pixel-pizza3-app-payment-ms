2573eb0d75da89d22543dd9ecb217d22731674be
Anderson Silva <anderson@andersonalves.com.br>
2024-01-05 18:27:30 -0300

    Inclusão k8s completo

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ dotenv.config({
   path: '.env'
 })
 
+console.log('API API_ORDER_BASEURL', process.env.API_ORDER_BASEURL)
+console.log('API API_PAYMENT_BASEURL', process.env.API_PAYMENT_BASEURL)
+console.log('API API_PRODUCTION_BASEURL', process.env.API_PRODUCTION_BASEURL)
+
 const dbconnectionMysql = new MysqldbConnection();
 const fastfoodApp = new FastfoodApp(dbconnectionMysql);
 fastfoodApp.start();
